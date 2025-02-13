23b1246167530530fe6a8a515567d3606aa3558f
Wesenheit <mr.kapusta@student.uw.edu.pl>
2025-02-13 12:33:28 +0100

    small fix

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ args = ["-O3","-march=native","-g"]
 wraper = Extension("FAOV",sources=["src/python_wrapper.c","src/FAOV.c"],extra_compile_args = args, include_dirs = [numpy.get_include()])
 
 setup(
-    name = "FAOV",
-    version = "0.1",
-    maintainer = "Mateusz Kapusta",
-    author_email ="mr.kapusta@student.uw.edu.pl",
     install_requires = [
             "numpy>=1.22.3",
         ],
