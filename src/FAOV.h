3eb1d8dd95aa13e0a43035991b12a40ddf7d25e4
Mateusz Kapusta <mitra@Voyager-2.local>
2025-06-18 00:11:58 +0200

    Add formating

--- src/FAOV.h
+++ src/FAOV.h
@@ -1,17 +1,18 @@
-#include <math.h>
 #include <immintrin.h>
+#include <math.h>
 #define NMAX 20000
 
-
-typedef union __attribute__ ((aligned (sizeof(__m256d))))
-{
-    __m256d simd_vector;
-    double array[4];
+typedef union __attribute__((aligned(sizeof(__m256d)))) {
+  __m256d simd_vector;
+  double array[4];
 } Vector;
 /*
-Most important data structure for the program. Union that represents four doubles.
+Most important data structure for the program. Union that represents four
+doubles.
 */
 
-double AOV(Vector* magnitude, Vector* time, int n, int bins,double freq,double mean);
-double period(double *time, double *magnitude, int n,int bins,double t_max);
-double period_time_domain(double *time, double *magnitude, int n,int bins,double t_min,double t_max,int num_points);
+double AOV(Vector *magnitude, Vector *time, int n, int bins, double freq,
+           double mean);
+double period(double *time, double *magnitude, int n, int bins, double t_max);
+double period_time_domain(double *time, double *magnitude, int n, int bins,
+                          double t_min, double t_max, int num_points);
--- test/test_python.py
+++ test/test_python.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from FAOV import Period,Power
+from FAOV import Period, Power
+
 name_source = "OGLE-BLG-RRLYR-00445.dat"
-t = np.loadtxt(name_source,usecols=[0])
-mag = np.loadtxt(name_source,usecols=[1])
-per = Period(t,mag,10,-1.)
-freq, power = Power(t,mag,10,-1.)
-print("freq arr: ",freq)
-print("power arr: ",power)
-print("period found: ",per)
-phase = np.modf((t-t[0])/per)[0]
-fig,(ax1,ax2) = plt.subplots(1,2,figsize = (12,6))
-ax1.scatter(phase,mag,color = "black",marker = ".")
-ax2.plot(freq,power,color = "black")
+t = np.loadtxt(name_source, usecols=[0])
+mag = np.loadtxt(name_source, usecols=[1])
+per = Period(t, mag, 10, -1.0)
+freq, power = Power(t, mag, 10, -1.0)
+print("freq arr: ", freq)
+print("power arr: ", power)
+print("period found: ", per)
+phase = np.modf((t - t[0]) / per)[0]
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+ax1.scatter(phase, mag, color="black", marker=".")
+ax2.plot(freq, power, color="black")
 ax1.set_xlabel("Time [d]")
 ax1.set_ylabel("Magnitude")
 ax2.set_xlabel("Frequency [1/d]")
